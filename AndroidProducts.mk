272b8a26744a1561a5d13321a0a6243cb1b16fc4
Aoihara <veronicadisa6@gmail.com>
2020-03-19 08:15:27 +0100

    land: Komodo-OS bringup
    Signed-off-by: Aoihara <veronicadisa6@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_land.mk
+    $(LOCAL_DIR)/komodo_land.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_land-user \
-    aosp_land-userdebug \
-    aosp_land-eng
+    komodo_land-user \
+    komodo_land-userdebug \
+    komodo_land-eng
