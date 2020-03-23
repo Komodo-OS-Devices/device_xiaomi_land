d33c3dc38efc180e5ef789ed61b2f582e7fd2791
Aoihara <veronicadisa6@gmail.com>
2020-03-23 10:37:43 +0100

    land: Reduce jitter and Make UI more smooth
    - Original commit -
    https://source.codeaurora.org/quic/la/device/google/marlin/commit/?h=android-9.0.0_r54&id=61a8a5af0242d27d01a8f8fae5147fb487d32379

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -213,8 +213,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
